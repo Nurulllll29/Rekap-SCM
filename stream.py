afd378de677b5a031a1fe8518c0bbd11d2239fc5
Analyst-FPnA <analyst.fpna@gmail.com>
2025-05-19 11:56:55 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -13,7 +13,7 @@ from xlsxwriter import Workbook
 import tempfile
 import re
 import io
-  
+
 def load_excel(file_path):
     with open(file_path, 'rb') as file:
         model = pd.read_excel(file, engine='openpyxl')
